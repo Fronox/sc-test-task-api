dfdddfae78f83d92c6bd910f835f3af38e429f86
fronox <serjzv@gmail.com>
2019-11-20 01:54:05 +0200

    .env is added

--- src/server.js
+++ src/server.js
@@ -3,20 +3,24 @@ const parser = require("body-parser");
 const multer = require('multer');
 const aws = require('aws-sdk');
 const multerS3 = require('multer-s3');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const cors = require('cors');
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
-const port = 4000;
-const cors = require('cors');
+const port = process.env.PORT;
+app.use(parser.urlencoded({extended: false, limit: '50mb'}));
+app.use(parser.json());
+app.use(cors());
+
 const s3 = new aws.S3({
     accessKeyId: process.env.AWS_IAM_USER_KEY,
     secretAccessKey: process.env.AWS_IAM_USER_SECRET,
     Bucket: process.env.AWS_BUCKET_NAME
-})
+});
 
-const upload = multer({
+const uploadS3 = multer({
     storage: multerS3({
         s3: s3,
         bucket: process.env.AWS_BUCKET_NAME,
@@ -27,17 +31,10 @@ const upload = multer({
             cb(null, Date.now() + '.mp4')
           }
     })
-})
+});
 
-
-app.use(parser.urlencoded({extended: false,limit: '50mb'}));
-app.use(parser.json());
-app.use(cors())
-
-
-app.post("/api/v1/video", upload.single('file'), (req, res, next) => {
+app.post("/api/v1/video", uploadS3.single('file'), (req, res, next) => {
     res.send("The video is uploaded!")
 });
 
-
-app.listen(port, () => console.log(`Api is listening on post ${port}`))
+app.listen(port, () => console.log(`Api is listening on post ${port}`));
